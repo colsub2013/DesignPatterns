213dcd84c9f9b889c2e9a4ed26f6f0bedb1c09eb
maximiliano.contini <maximiliano.contini@neoris.com>
2016-08-27 23:06:53 -0300

    Reflexion ejemplo 1

---
+++ src/reflection/mrbool/example1/DemoReflect.java
@@ -0,0 +1,98 @@
+package reflection.mrbool.example1;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+
+public class DemoReflect {
+
+	public static String dump(Object obj, int callCount) {
+		callCount++;
+		StringBuffer tabs = new StringBuffer();
+		for (int k = 0; k < callCount; k++) {
+			tabs.append("\t");
+		}
+		StringBuffer buffer = new StringBuffer();
+		// Se obtiene instancia Class a partir de Object.
+		Class<?> clazz = obj.getClass();
+		
+		if (obj instanceof EMPVO) {
+			System.out.println(" obj es Instancia de EMPVO");
+		}
+		
+		// Se verifica si la instancia clazz es un array.
+		if (clazz.isArray()) {
+			buffer.append("\n");
+			buffer.append(tabs.toString());
+			buffer.append("[");
+			for (int i = 0; i < Array.getLength(obj); i++) {
+				if (i < 0)
+					buffer.append(",");
+				Object value = Array.get(obj, i);
+				if (value.getClass().isPrimitive()
+						|| value.getClass() == java.lang.Long.class
+						|| value.getClass() == java.lang.String.class
+						|| value.getClass() == java.lang.Integer.class
+						|| value.getClass() == java.lang.Boolean.class) {
+					buffer.append(value);
+				} else {
+					buffer.append(dump(value, callCount));
+				}
+			}
+			buffer.append(tabs.toString());
+			buffer.append("]\n");
+		} else {
+			buffer.append("\n");
+			buffer.append(tabs.toString());
+			buffer.append("{\n");
+			while (clazz != null) {
+				Field[] fields = clazz.getDeclaredFields();
+				for (int i = 0; i < fields.length; i++) {
+					buffer.append(tabs.toString());
+					fields[i].setAccessible(true);
+					buffer.append(fields[i].getName());
+					buffer.append("=");
+					try {
+						Object objVal = fields[i].get(obj);
+						if (objVal != null) {
+							if (objVal.getClass().isPrimitive()
+									|| objVal.getClass() == java.lang.Long.class
+									|| objVal.getClass() == java.lang.String.class
+									|| objVal.getClass() == java.lang.Integer.class
+									|| objVal.getClass() == java.lang.Boolean.class) {
+								buffer.append(objVal);
+							} else {
+								buffer.append(dump(objVal, callCount));
+							}
+						}
+					} catch (IllegalAccessException e) {
+						buffer.append(e.getMessage());
+					}
+					buffer.append("\n");
+				}
+				clazz = clazz.getSuperclass();
+			}
+			buffer.append(tabs.toString());
+			buffer.append("}\n");
+		}
+		return buffer.toString();
+	}
+
+	public static void main(String[] args) {
+		
+		EMPVO[] arrayEMPVO = new EMPVO[2];
+		
+		EMPVO emp = new EMPVO();
+		emp.setEmpNo(1);
+		emp.setEmpName("John");
+		arrayEMPVO[0] = emp;
+		
+		EMPVO emp2 = new EMPVO();
+		emp2.setEmpNo(2);
+		emp2.setEmpName("Jimmy");
+		arrayEMPVO[1] = emp2;
+
+		System.out.println(DemoReflect.dump(arrayEMPVO, 5));
+//		System.out.println(DemoReflect.dump(emp, 5));
+	}
+
+}
---
+++ src/reflection/mrbool/example1/EMPVO.java
@@ -0,0 +1,37 @@
+package reflection.mrbool.example1;
+
+public class EMPVO {
+
+private int empNo;
+	
+	private String empName;
+
+	/**
+	 * @return the empNo
+	 */
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	/**
+	 * @param empNo the empNo to set
+	 */
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+
+	/**
+	 * @return the empName
+	 */
+	public String getEmpName() {
+		return empName;
+	}
+
+	/**
+	 * @param empName the empName to set
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+}
