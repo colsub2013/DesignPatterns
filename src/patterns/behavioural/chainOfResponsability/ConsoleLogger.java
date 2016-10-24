99c16d31952b03d320d965efea5e32f72e5d55c7
maximiliano.contini <maximiliano.contini@neoris.com>
2016-10-23 21:26:54 -0300

    Chain of responsability

---
+++ src/patterns/behavioural/chainOfResponsability/ConsoleLogger.java
@@ -0,0 +1,13 @@
+package patterns.behavioural.chainOfResponsability;
+
+public class ConsoleLogger extends AbstractLogger {
+
+	public ConsoleLogger(int level) {
+		this.level = level;
+	}
+
+	@Override
+	protected void write(String message) {
+		System.out.println("Standard Console::Logger: " + message);
+	}
+}
